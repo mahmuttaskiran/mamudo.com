167fd45667288dca77df62e97f1d3e6057283b33
Mahmut Taşkıran <taskiranmahmutt@gmail.com>
2022-11-15 21:57:08 +0300

    flutter build web --web-renderer canvaskit

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "568d56b09f9795dd12c27aeb041db878",
-"index.html": "581c96a79e65b80f7273b3d847a7d5f3",
-"/": "581c96a79e65b80f7273b3d847a7d5f3",
-"main.dart.js": "0268b363a56460f57830408b923955b1",
+  "version.json": "666924dd052f6b233c4c35404a91b552",
+"index.html": "ff1ba490bb897cf87967d8b707daa8de",
+"/": "ff1ba490bb897cf87967d8b707daa8de",
+"main.dart.js": "cbb9bd94d08ad395608431afc124922a",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "1204d469df2daf2943f170e70e101660",
 "cv.pages": "0fb7d045eb44efa9f4da6fb9ead0f02a",
@@ -31,7 +31,6 @@ const RESOURCES = {
 "assets/assets/localizations/tr.json": "6abb7c8aa5592caf5c2b12147592ce26",
 "assets/assets/localizations/en.json": "d957745e9b8fd4ef41b30c33964e4b8c",
 "mahmuttaskiran_tr.pages": "c87e3c6ade693ba44465d54b0b3086bb",
-"cv.pdf": "0abdf82d7e86188f8c9f1404710930dc",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
 "canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
 "canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
